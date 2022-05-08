78383065cc6e9baea49fe81f1b2fc50dd249e77c
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-08 15:10:01 -0500

    Java 9 TypeCasting: Situacion formulada en la clase Cumpleanios

---
+++ 9TypeCasting/Cumpleanios.java
@@ -0,0 +1,17 @@
+// CUmpleanios:
+// Programa que reparte un pastel de cierto peso entre una cantidad dada de personas de tal manera que se
+// reparten las porciones en uno de los siguientes casos:
+//  - Porciones de peso decimal iguales para todos
+//  - Porciones de peso entero y lo que sobre se le agrega a la porcion del cumpleaniero
+// La funcion repartirPasteles() recibe:
+//  - El peso del pastel en gramos
+//  - La cantidad de asistentes
+//  - Booleano: Falso (Peso decimal) ; Verdadero (Peso entero)
+// y devuelve un arreglo con los pesos de pastel para cada asistente, siendo la primera porcion para el
+// cumpleaniero
+public class Cumpleanios{
+	public static void main(String[] args){
+		
+	}
+}
+
