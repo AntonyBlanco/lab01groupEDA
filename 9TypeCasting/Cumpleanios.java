a927ffee3dd2f38274d128835b3cac0e938dad1b
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-09 10:02:21 -0500

    Java 9 TypeCasting: Agregado el prompt para el usuario

--- 9TypeCasting/Cumpleanios.java
+++ 9TypeCasting/Cumpleanios.java
@@ -1,4 +1,4 @@
-// CUmpleanios:
+// Cumpleanios:
 // Programa que reparte un pastel de cierto peso entre una cantidad dada de personas de tal manera que se
 // reparten las porciones en uno de los siguientes casos:
 //  - Porciones de peso decimal iguales para todos
@@ -11,7 +11,33 @@
 // cumpleaniero
 public class Cumpleanios{
 	public static void main(String[] args){
-		
+		System.out.println("  ---- Birthday Party ----");
+
+		System.out.print("\nInsert cake weight: ");
+		double cakeWeight = 40.0; // Peso del pastel
+		System.out.print("\nCake weight: " + cakeWeight);
+
+		System.out.print("\nInsert assistants quantity: ");
+		int qtyAssistants = 12; // Cantidad de asistentes
+		System.out.print("\nAssistants quantity: " + qtyAssistants);
+
+		System.out.print("\nDivide in decimal or integer parts? (d/i): ");
+		char diInput = 'd';
+		boolean boolDecOrInt = false; // Peso decimal o entero
+		if(diInput == 'd'){ // Particion modo Decimal
+			boolDecOrInt = false;
+			System.out.print("\nSet partition mode to Decimal");
+		} else if(diInput == 'i'){ // Particion modo Integer
+			boolDecOrInt = true;
+			System.out.print("\nSet partition mode to Integer");
+		}
+		System.out.println();
+
+		// Porciones a distribuir
+		double[] portions = repartirPasteles(cakeWeight, qtyAssistants, boolDecOrInt);
+	}
+	public static double[] repartirPasteles(double cakeW, int qtyPeople, boolean decimalOrInteger){
+		return null;
 	}
 }
 
