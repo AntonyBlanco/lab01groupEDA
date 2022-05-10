5f2395073465a41380f432fab15e55ff00793651
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 16:19:52 -0500

    Java 12 Math: Se crea la clase RandomArrayGenerator que implementará funciones para generar arreglos de valores aleatorios

---
+++ 12Math/RandomArrayGenerator.java
@@ -0,0 +1,8 @@
+// Esta clase implementa 2 funciones que generan un arreglo de enteros y de reales
+public class RandomArrayGenerator{
+	public static void main(String[] args){
+		System.out.println("Random Array Generator");
+	}
+
+}
+
