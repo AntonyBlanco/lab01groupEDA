e7b1bb052a0986bf3c825b2a90debe5fce611622
Edisson Franklin <69181980+Edisson-Acc-UNSA@users.noreply.github.com>
2022-05-10 08:59:46 -0500

    bucle For y for each
    En esta seccion vamos a desarrollar ejemplos básicos del bucle for y una variante que resume algunas lineas, sin embargo solo es relevante cuando queremos enlistar matrices (for each)

---
+++ 17ForLoop/ForEachLoop.java
@@ -0,0 +1,11 @@
+package ForLoop;
+
+public class ForEachLoop {
+	public static void main(String[] args) {
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		for (String i : cars) {
+		  System.out.println(i);
+		}
+	}
+
+}
---
+++ 17ForLoop/ForLoop.java
@@ -0,0 +1,10 @@
+package ForLoop;
+//Bucle for 
+public class ForLoop {
+	public static void main(String[] args) {
+		int limite =50;
+		for (int i = 0; i < limite; i++) {
+			System.out.print(i+1+"-"); //imprimimos numeros entre 1 y el limite (50 para este caso)
+		}
+	}
+}
