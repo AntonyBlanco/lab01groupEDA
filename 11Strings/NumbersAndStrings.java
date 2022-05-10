9259f3ebae01253f3ccb74557400a41f4aef225e
Edisson Franklin <69181980+Edisson-Acc-UNSA@users.noreply.github.com>
2022-05-10 06:44:14 -0500

    concatenacion de numeros y cadenas
    En esta sección hacemos ejemplos de concatenación de cadenas y numeros, haciendo notar que no debemos olvidar que el operador + sirve para concatenar y tambien para sumar lo que a veces podria generar errores en la codificación

---
+++ 11Strings/NumbersAndStrings.java
@@ -0,0 +1,12 @@
+package Strings;
+
+//Concatenacion de numeros y cadenas
+public class NumbersAndStrings {
+	public static void main(String[] args) {
+		int numero=10;
+		String numeroTxt = "11";
+		//el operador "+" se usa tanto como para sumar  y tambien para concatenar, por ello debemos de tener cuidado a la hora de 
+		//la salida de una impresion
+		System.out.println(numero + numeroTxt);
+	}
+}
