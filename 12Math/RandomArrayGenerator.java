5739647930a80e2c8260223c2f65d05615ff38a4
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 16:30:50 -0500

    Java 12 Math: Se implementan las funciones genIntArray() que genera un arreglo de valores enteros aleatorios, y printArr() para mostrar un arreglo de enteros

--- 12Math/RandomArrayGenerator.java
+++ 12Math/RandomArrayGenerator.java
@@ -2,7 +2,29 @@
 public class RandomArrayGenerator{
 	public static void main(String[] args){
 		System.out.println("Random Array Generator");
+		int[] intArr = genIntArray(10, -5, 15);
+		printArr(intArr);
 	}
-
+	public static int[] genIntArray(int size, int min, int max){
+	// Genera un arreglo de enteros
+	// "size" es el tamaño del arreglo
+	// "min" es el minimo valor aleatorio, el valor es incluido
+	// "max" es el máximo valor aleatorio, el valor es  excluido
+		int[] arr = new int[size];
+		for(int i = 0; i < size; i++){
+			arr[i] = (int)(Math.random() * (max - min) + min);
+		}
+		return arr;
+	}
+	public static void printArr(int[] arr){
+	// Imprime un arreglo de enteros
+		System.out.print("[");
+		for(int i = 0; i < arr.length; i++){
+			if(i != 0)
+				System.out.print(",");
+			System.out.print( arr[i] );
+		}
+		System.out.println("]");
+	}
 }
 
