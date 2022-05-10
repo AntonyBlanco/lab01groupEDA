33016ffbb8d35a90f439805fd4a5f2175cf2b9a6
Edisson Franklin <69181980+Edisson-Acc-UNSA@users.noreply.github.com>
2022-05-10 06:45:36 -0500

    caracteres especiales
    En esta sección vemos el uso de caacteres especiales que permiten saltos de linea, tabulaciones y demás

---
+++ 11Strings/SpecialCharacters.java
@@ -0,0 +1,18 @@
+package Strings;
+
+//Manejo de caracteres especiales
+public class SpecialCharacters {
+	public static void main(String[] args) {
+		System.out.println("CARACTERES ESPECIALES");
+		//para insertar, comillas simples, comillas dobles y barra invertida(contra slash)
+		System.out.println("\'tú cadena\' ");
+		System.out.println("\"tú cadena\"");
+		System.out.println("\\tú cadena\\");
+		//otros caracteres especiales
+		System.out.println("OTROS CARACTERES ESPECIALES");
+		System.out.println("Hola \namigos"); //insertar salto de linea 
+		System.out.println("Hola\ramigos");
+		System.out.println("Hola \t\tamigos");
+		System.out.println("Holaa\bmigos");
+	}
+}
