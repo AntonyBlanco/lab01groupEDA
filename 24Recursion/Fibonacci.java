9e818a1104622c60ebb9ec678d5f33a1151be50c
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 23:05:04 -0500

    Java 24 Recursion: Implementado fibonacci()

--- 24Recursion/Fibonacci.java
+++ 24Recursion/Fibonacci.java
@@ -3,5 +3,12 @@ public class Fibonacci{
 	public static void main(String[] args){
 		System.out.println(" == Fibonacci == ");
 	}
+	public static int fibonacci(int n){
+		if(n == 0 || n == 1){
+			return n;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
 }
 
