577b941b2625fd597d9d08ddae371fdbe6a5f28c
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:04:08 -0500

    Java 12 Math: Se utiliza entrada estandar para ingreso de informacion

--- 12Math/Pitagoras.java
+++ 12Math/Pitagoras.java
@@ -1,13 +1,17 @@
 // El programa pide 2 lados de un triangulo (double o in) para hallar la hipotenusa del triangulo
+import java.util.Scanner;
+
 public class Pitagoras{
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println(" == Pitagoras == ");
 		System.out.print("\nIngrese cateto 1: ");
-		double cat1 = 40;
+		double cat1 = sc.nextDouble();
 		System.out.println("\nCateto 1 = " + cat1);
 
 		System.out.print("\nIngrese cateto 2: ");
-		double cat2 = 9;
+		double cat2 = sc.nextDouble();
 		System.out.println("\nCateto 2 = " + cat2);
 
 		double hip = pitagoras(cat1, cat2);
