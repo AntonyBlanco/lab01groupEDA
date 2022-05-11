73917d344fb56bc42b57e4e107703f51f925e7ee
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 23:08:09 -0500

    Java 24 Recursion: Caso demostrativo de fibonacci usando entrada estandar para el usuario

--- 24Recursion/Fibonacci.java
+++ 24Recursion/Fibonacci.java
@@ -1,7 +1,17 @@
 // Devolver el elemento n de la sucesion de Fibonacci para f(0) = 0 y f(1) = 1
+import java.util.Scanner;
+
 public class Fibonacci{
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println(" == Fibonacci == ");
+		System.out.print("Ingrese valor para n de fibonacci: ");
+		int n = sc.nextInt();
+		int fibonacciN = fibonacci(n);
+
+		System.out.println("Elemento " + n + " de Fibonacci:");
+		System.out.println(fibonacciN);
 	}
 	public static int fibonacci(int n){
 		if(n == 0 || n == 1){
