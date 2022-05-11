f5a17a2af59d3ce86a4fc5fb1897634404253640
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:50:53 -0500

    Java 18 Continue: Se implementa la funcion repartirPeriodicos() para mostrar las entregas, se corrigio la asignacion de valores en la funcion generateBooleanArray()

--- 18BreakContinue/Repartidor.java
+++ 18BreakContinue/Repartidor.java
@@ -4,18 +4,30 @@
 public class Repartidor{
 	public static void main(String[] args){
 		System.out.println(" == Repartidor de Periodicos ==");
+		boolean[] clientes = generateBooleanArray(15);
+		repartirPeriodicos(clientes);
 	}
 	public static boolean[] generateBooleanArray(int size){
 	// Genera un arreglo de booleanos aleatorios
 		boolean[] arr = new boolean[size];
 		for(int i = 0; i < size; i++){
-			arr[0] = generateRndBoolean();
+			arr[i] = generateRndBoolean();
 		}
 		return arr;
 	}
 	public static boolean generateRndBoolean(){
 	// Genera un booleano aleatorio
 		return Math.random() < 0.5 ? false : true;
+	}
+	public static void repartirPeriodicos(boolean[] lista){
+	// Muestra en la salida estandar el proceso de las entregas
+		for(int i = 0; i < lista.length; i++){
+			boolean pagado = lista[i];
+			if(!pagado) // Si no ha pagado se dirige al siguiente cliente
+				continue;
+			System.out.println("Periódico entregado a cliente " + i);
+		}
+		System.out.println("Entregas terminadas.");
 	}
 }
 
