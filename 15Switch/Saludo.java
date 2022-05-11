94c42ea3079395fc4fef384a9769d3a66ea519da
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:19:38 -0500

    Java 15 Switch: Se plantea la situación del saludo de distintos animales

---
+++ 15Switch/Saludo.java
@@ -0,0 +1,10 @@
+// Esta clase pregunta al usuario si quiere recibir el saludo de distintos animales
+// Ejemplo de uso de Switch
+
+
+public class Saludo{
+	public static void main(String[] args){
+		System.out.println("Saludo");
+	}
+}
+
