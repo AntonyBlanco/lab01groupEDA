c84cd1e09560612e959027e50b6577a3b4b33894
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:40:18 -0500

    Java 18 Continue: Se plantea la situacion de un repartidor que entrega solo a quienes pagaron

---
+++ 18BreakContinue/Repartidor.java
@@ -0,0 +1,10 @@
+// Un repartidor de periodicos solo entrega el periodico a quienes pagaron por el
+//
+// Clientes se representan en un arreglo de booleanos
+public class Repartidor{
+	public static void main(String[] args){
+		System.out.println(" == Repartidor de Periodicos ==");
+	}
+
+}
+
