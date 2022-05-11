e7ae323267021bfc2e135a61c22c959d36673f82
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 20:26:08 -0500

    Java 12 Math: Se termina la impementación de minValueSearch() y se implementa printArr en la clase MinValueSearch

--- 12Math/MinValueSearch.java
+++ 12Math/MinValueSearch.java
@@ -4,12 +4,27 @@ public class MinValueSearch{
 		System.out.println("Mininum Value Search");
 		// Genera un arreglo de valores enteros aleatorios [0; 50>
 		int[] arr = RandomArrayGenerator.genIntArray(20, 0, 50);
-		RandomArrayGenerator.printArr(arr);
+		printArr(arr);
 		int minVal = minValueSearch(arr);
 		System.out.println("Minimun value of array: " + minVal);
 	}
 	public static int minValueSearch(int[] arr){
-		return arr[0];
+		int min = arr[0];
+		for(int i = 1; i < arr.length; i++){
+			// Establece el minimo valor
+			min = Math.min(min, arr[i]);
+		}
+		return min;
+	}
+	public static void printArr(int[] arr){
+	// Imprime un arreglo de enteros
+		System.out.print("[");
+		for(int i = 0; i < arr.length; i++){
+			if(i != 0)
+				System.out.print(",");
+			System.out.print( arr[i] );
+		}
+		System.out.println("]");
 	}
 }
 
