298817aa41a2f255755f85a01a37aa32c40425dd
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 20:22:37 -0500

    Java 12 Math: Se crea la función minValueSearch() por implementar, se hace avance en la función main() para la demostracion

--- 12Math/MinValueSearch.java
+++ 12Math/MinValueSearch.java
@@ -2,7 +2,14 @@
 public class MinValueSearch{
 	public static void main(String[] args){
 		System.out.println("Mininum Value Search");
+		// Genera un arreglo de valores enteros aleatorios [0; 50>
+		int[] arr = RandomArrayGenerator.genIntArray(20, 0, 50);
+		RandomArrayGenerator.printArr(arr);
+		int minVal = minValueSearch(arr);
+		System.out.println("Minimun value of array: " + minVal);
 	}
-
+	public static int minValueSearch(int[] arr){
+		return arr[0];
+	}
 }
 
