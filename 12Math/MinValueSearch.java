0bfc19980dd95db1c11d7d3db666b9b2a71d784a
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 20:18:21 -0500

    Java 12 Math: Se crea la clase MinValueSearch para demostración de Math.min()

---
+++ 12Math/MinValueSearch.java
@@ -0,0 +1,8 @@
+// Esta clase muestra un ejemplo de Math.min() para buscar el minimo valor de un arreglo
+public class MinValueSearch{
+	public static void main(String[] args){
+		System.out.println("Mininum Value Search");
+	}
+
+}
+
