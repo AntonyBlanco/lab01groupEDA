6ad1c277ddb74c1d9406ce0b60db8468169f0411
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 21:01:34 -0500

    Java 12 Math: Se plantea el problema de la hipotenusa con Teorema de pitagoras en la clase nueva Pitagoras

---
+++ 12Math/Pitagoras.java
@@ -0,0 +1,7 @@
+// El programa pide 2 lados de un triangulo (double o in) para hallar la hipotenusa del triangulo
+public class Pitagoras{
+	public static void main(String[] args){
+		System.out.println("Pitagoras");
+	}
+}
+
