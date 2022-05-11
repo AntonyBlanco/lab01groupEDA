f3c8d814843d35481d708e67b4d1656d878ff46f
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:30:53 -0500

    Java 18 Break: Se plantea la situacion de recibir caramelos segun la edad ingresada

---
+++ 18BreakContinue/Caramelos.java
@@ -0,0 +1,9 @@
+// Al salir de una fiesta, recibes tantos caramelos como años te falten para tener 30
+
+public class Caramelos{
+	public static void main(String[] args){
+
+		System.out.println(" == Caramelos! ==");
+	}
+}
+
