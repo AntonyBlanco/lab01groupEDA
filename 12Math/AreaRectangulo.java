8297382797ac3d4f127eae8ff56e7ab2232e9577
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:14:06 -0500

    Java 12 Math: Se desarrolla main() creando un prompt para la entrada estandar, creado calcularAreaRect() ṕor implementar

--- 12Math/AreaRectangulo.java
+++ 12Math/AreaRectangulo.java
@@ -1,10 +1,26 @@
 // Se ingresan 2 puntos opuestos (x, y) de un rectangulo en el espacio 2D
 // El programa calcula el área de dicho rectangulo
-
+import java.util.Scanner;
 
 public class AreaRectangulo{
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println(" == Area de Rectangulo == ");
+		
+		System.out.println("\nIngrese 2 numeros para P1(x,y):");
+		double xP1 = sc.nextDouble();
+		double yP1 = sc.nextDouble();
+
+		System.out.println("\nIngrese 2 numeros para P2(x,y):");
+		double xP2 = sc.nextDouble();
+		double yP2 = sc.nextDouble();
+		
+		double area = calcularAreaRect(xP1, yP1, xP2, yP2);
+		System.out.println("\nArea calculara: " + area);
+	}
+	public static double calcularAreaRect( double xP1, double yP1, double xP2, double yP2){
+		return 0;
 	}
 }
 
