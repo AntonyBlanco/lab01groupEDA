327f1412e9f3d32d9f9686c72325f3cefc61d0a7
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:07:17 -0500

    Java 12 Math: Se plantea el problema de calcular el area de un rectangulo dados 2 puntos, en la clase AreaRectangulo

---
+++ 12Math/AreaRectangulo.java
@@ -0,0 +1,10 @@
+// Se ingresan 2 puntos opuestos (x, y) de un rectangulo en el espacio 2D
+// El programa calcula el área de dicho rectangulo
+
+
+public class AreaRectangulo{
+	public static void main(String[] args){
+		System.out.println(" == Area de Rectangulo == ");
+	}
+}
+
