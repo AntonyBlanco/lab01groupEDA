977bb43b932d3021be0492ddf4ce5eafa9ad52eb
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:56:18 -0500

    Java 21 MethodParams: Se plantea la multiplicacion sin utilizar el operador multiplicador

---
+++ 21MethodParameters/Multiplicacion.java
@@ -0,0 +1,8 @@
+// Un método que devuelva el producto de 2 numeros enteros sin usar el operador de producto
+public class Multiplicacion{
+	public static void main(String[] args){
+		System.out.println(" == Multiplicacion ==");
+	}
+
+}
+
