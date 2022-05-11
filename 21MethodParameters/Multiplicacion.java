54656348142f80f8df573710c23197e8a82c2179
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 23:00:57 -0500

    Java 21 MethodParams: Correcion de errores de producto(), y demostracion de uso en main()

--- 21MethodParameters/Multiplicacion.java
+++ 21MethodParameters/Multiplicacion.java
@@ -2,13 +2,16 @@
 public class Multiplicacion{
 	public static void main(String[] args){
 		System.out.println(" == Multiplicacion ==");
+		System.out.println("Producto de 13 y 27");
+		int res = producto(13, 27);
+		System.out.println("Resultado: " + res);
 	}
-	public static int producto(int a. int b){
+	public static int producto(int a, int b){
 		int prod = 1; // Elemento neutro de operacion producto
 		for(int i = 0; i < b; i++){
 			prod += a;
 		}
-		return prod
+		return prod;
 	}
 }
 
