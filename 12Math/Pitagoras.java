2151c7cb30e4a97f36cec5a915fc0543ddee0896
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:00:46 -0500

    Java 12 Math: Creado prompt para usuario, con valores predefinidos

--- 12Math/Pitagoras.java
+++ 12Math/Pitagoras.java
@@ -1,7 +1,21 @@
 // El programa pide 2 lados de un triangulo (double o in) para hallar la hipotenusa del triangulo
 public class Pitagoras{
 	public static void main(String[] args){
-		System.out.println("Pitagoras");
+		System.out.println(" == Pitagoras == ");
+		System.out.print("\nIngrese cateto 1: ");
+		double cat1 = 40;
+		System.out.println("\nCateto 1 = " + cat1);
+
+		System.out.print("\nIngrese cateto 2: ");
+		double cat2 = 9;
+		System.out.println("\nCateto 2 = " + cat2);
+
+		double hip = pitagoras(cat1, cat2);
+		System.out.println("\nHipotenusa calculada: " + hip);
+		
+	}
+	public static double pitagoras(double a, double b){
+		return 0;
 	}
 }
 
