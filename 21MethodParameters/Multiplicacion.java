72aaa80b8215135f7c69af8f3aa96c24e1db3685
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-10 22:58:10 -0500

    Java 21 MethodParams: Implementado producto()

--- 21MethodParameters/Multiplicacion.java
+++ 21MethodParameters/Multiplicacion.java
@@ -3,6 +3,12 @@ public class Multiplicacion{
 	public static void main(String[] args){
 		System.out.println(" == Multiplicacion ==");
 	}
-
+	public static int producto(int a. int b){
+		int prod = 1; // Elemento neutro de operacion producto
+		for(int i = 0; i < b; i++){
+			prod += a;
+		}
+		return prod
+	}
 }
 
